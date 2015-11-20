05d49975e79a3c1abcdddd8bdf8d7b2d0ee5329a
Confikt <confikt@gmail.com>
2015-11-20 13:25:30 +0100

    Rask fiks for å se gamet

--- src/main/java/no/hig/hers/ludoclient/HostGameLobbyController.java
+++ src/main/java/no/hig/hers/ludoclient/HostGameLobbyController.java
@@ -1,8 +1,12 @@
 package no.hig.hers.ludoclient;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.control.Button;
 
 public class HostGameLobbyController {
@@ -26,7 +30,14 @@ public class HostGameLobbyController {
 	
 	
 	@FXML private void startGameButtonPressed(ActionEvent e) {
+		Tab tab = Main.gameTabs.getTabs().get(1);
+		FXMLLoader loader = new FXMLLoader();
 		
+		try {
+			tab.setContent(loader.load(getClass().getResource("GameClient.fxml").openStream()));
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
 	
 	public void getServerPort(int serverPort) {
