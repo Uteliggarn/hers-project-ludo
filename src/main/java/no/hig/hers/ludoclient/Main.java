898ca46b400f61ee1f4e23cc5b080932b88b3c8f
Hauken1 <henrik.hauk.92@gmail.com>
2015-11-24 16:40:39 +0100

    test

--- src/main/java/no/hig/hers/ludoclient/Main.java
+++ src/main/java/no/hig/hers/ludoclient/Main.java
@@ -103,8 +103,8 @@ public class Main extends Application {
 	 */
 	public static void connect() {
 		try {
-			//connection = new Socket("128.39.83.87", 12344);
-			connection = new Socket("127.0.0.1", 12344);
+			connection = new Socket("128.39.83.87", 12344);
+			//connection = new Socket("127.0.0.1", 12344);
 			
 			output = new BufferedWriter(new OutputStreamWriter(
                     connection.getOutputStream()));
