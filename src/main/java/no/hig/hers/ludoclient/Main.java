89c5bde15ff1964ddb331234b31c96fa6405c73a
Hauken1 <henrik.hauk.92@gmail.com>
2015-11-24 13:06:18 +0100

    endret ip for testing

--- src/main/java/no/hig/hers/ludoclient/Main.java
+++ src/main/java/no/hig/hers/ludoclient/Main.java
@@ -105,7 +105,7 @@ public class Main extends Application {
 	
 	public static void connect() {
 		try {
-			connection = new Socket("127.0.0.1", 12344);
+			connection = new Socket(/*"127.0.0.1"*/"128.39.83.87", 12344);
 			
 			output = new BufferedWriter(new OutputStreamWriter(
                     connection.getOutputStream()));
