944caef6448412e8f70d1ba4bf93bda46b921365
Bjørn Nicholai Lindbach Endre <bne9988@gmail.com>
2015-11-25 10:20:03 +0100

    Added a hashtable for the filename, will use the same name as long as chat exists

--- src/main/java/no/hig/hers/ludoserver/GlobalServer.java
+++ src/main/java/no/hig/hers/ludoserver/GlobalServer.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.concurrent.*;
 
@@ -60,6 +61,7 @@ public class GlobalServer extends JFrame{
     
     private final String fileNameEnd = "ChatLog.log"; //The end of the filename
     private String fileName; //The whole filename
+    private Hashtable<String, String> hashtable;
     
     private int serverPorts = 10000;
     
