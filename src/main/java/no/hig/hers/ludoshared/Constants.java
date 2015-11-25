9383c0aa67b2cbeb702a37641a0ab49e559ffc79
Confikt <confikt@gmail.com>
2015-11-25 15:46:46 +0100

    Addet enda flere konster

--- src/main/java/no/hig/hers/ludoshared/Constants.java
+++ src/main/java/no/hig/hers/ludoshared/Constants.java
@@ -6,44 +6,31 @@ public class Constants {
 	public final static String ERRORCHAT = "ERRORCHAT";
 	public final static String LEAVECHAT = "LEAVE:";
 	public final static String CREATEGAME = "CREATEGAME";
-	public final static String IDGK = "IDGK";	// Unique name
-	public final static String INVITE = "INVITE:";	// Unique name
-	public final static String HOST = "HOST";	// Unique name
-	public final static String QUEUE = "QUEUE";	// Unique name
-	public final static String JOIN = "JOIN:";	// Unique name
+	public final static String IDGK = "IDGK";// Unique name
+	public final static String INVITE = "INVITE:";	
+	public final static String HOST = "HOST";	
+	public final static String QUEUE = "QUEUE";
+	public final static String JOIN = "JOIN:";	
 	public final static String QUITGAME = "LOGOUT:";
 	public final static String HOTJOIN = "HOTJOIN:";
-	public static final String TOPPLAYED = "TOPLISTPLAYED:";
-	public static final String TOPWON = "TOPLISTWON:";
+	public final static String TOPPLAYED = "TOPLISTPLAYED:";
+	public final static String TOPWON = "TOPLISTWON:";
 	public final static String GAMESTART = "GAMESTART:";
 	public final static String GAMENAME = "GAMENAME:";
 	public final static String DICEVALUE = "DICEVALUE:";
 	public final static String GAMEOVER = "GAMEOVER";
 	public final static String GLOBALCHAT = "GlobalJOIN:";	// Global chat name
-	public final static String QUEOPEN = "QUEOPEN";	// Global chat name
-	public final static String GAMEWON = "GAMEWON";	// Global chat name
-	public final static String GAMELOST = "GAMELOST";	// Global chat name
-	
-	/*"TOPLISTPLAYED:"
-	
-	private final String JOIN = "JOIN:";
-    private final String HOTJOIN = "HOTJOIN:";
-    private final String HOST = "HOST";
-    private final String INVITE = "INVITE:";	// invite: +7
-    private final String LOGOUT = "LOGOUT:";
-    private final String CLOGOUT = ">>>LOGOUT<<<";
-    private final String QUEUE = "QUEUE";
-    
-    private final String GLOBALCHAT = "GlobalJOIN:";	// Global chat name
-    
-    private final String IDGK = "IDGK";
-    private final String CREATEGAME = "CREATEGAME";
-    private final String ERROR = "ERROR";
-    private final String TOP = "TOP";
-    
-    private final String GWON = "GAMEWON";
-    private final String GLOST = "GAMELOST";
-	*/
+	public final static String QUEOPEN = "QUEOPEN";	
+	public final static String GAMEWON = "GAMEWON";	
+	public final static String GAMELOST = "GAMELOST";	
+	public final static String ACCEPTED = "ACCEPTED";
+	public final static String DECLINED = "DECLINED";
+	public final static String SENDLOGIN = "SENDLOGIN:";
+	public final static String SENDREGISTER = "SENDREGISTER:";
+	public final static String LOGOUT = "LOGOUT:";
+	public final static String CLOGOUT = ">>>LOGOUT<<<";
+	public final static String ERROR = "ERROR";
+	public final static String TOP = "TOP";
 	
 	private Constants() {}
 }
